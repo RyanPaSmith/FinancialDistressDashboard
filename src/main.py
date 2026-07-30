d73b408b62391d01ea85d3ff7d6ed05fbb0979ac
Cheyenne Zhang <czhang0135@gmail.com>
2026-07-29 22:11:23 -0400

    separation of concerns

--- src/main.py
+++ src/main.py
@@ -4,17 +4,23 @@ from src.dictionary import headers
 import json
 import pandas as pd
 
-""" ================================================= 
-    Get Apples Financial Data In One Giant Nested Dictionary
-    =================================================
-"""
-financial_data_apple = requests.get(financial_data_link,headers=headers)
-converted_financial_apple = json.loads(financial_data_apple.text)
 
-""" ================================================= 
-    Drill Down to Just Apples Assets and Put in Pandas DataFrame
-    =================================================
-"""
-converted_financial_apple.get("facts").get("us-gaap").get("Assets").get("units").get("USD")
-apple_assets_df = pd.DataFrame(converted_financial_apple)
-print(apple_assets_df)
+def main():
+    """ ================================================= 
+        Get Apples Financial Data In One Giant Nested Dictionary
+        =================================================
+    """
+    financial_data_apple = requests.get(financial_data_link,headers=headers)
+    converted_financial_apple = json.loads(financial_data_apple.text)
+
+    """ ================================================= 
+        Drill Down to Just Apples Assets and Put in Pandas DataFrame
+        =================================================
+    """
+    converted_financial_apple.get("facts").get("us-gaap").get("Assets").get("units").get("USD")
+    apple_assets_df = pd.DataFrame(converted_financial_apple)
+    print(apple_assets_df)
+
+
+if __name__ == '__main__':
+    main()
